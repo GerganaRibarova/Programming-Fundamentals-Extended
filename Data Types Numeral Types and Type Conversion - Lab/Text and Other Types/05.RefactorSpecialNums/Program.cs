98c1c7ae4abee2d64cca51e6f2de935a6cce0ee9
GerganaRibarova <geri.ribarova@gmail.com>
2017-01-26 22:01:05 +0200

    Add all exams from lab

---
+++ Data Types Numeral Types and Type Conversion - Lab/Text and Other Types/05.RefactorSpecialNums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _01.Special_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int num = 1; num <= n; num++)
+            {
+                int digit = num;
+                int sum = 0;
+                bool special = false;
+                while (digit != 0)
+                {
+                    sum += digit % 10;
+                    digit = digit / 10;
+                }
+                if (sum == 5 || sum == 7 || sum == 11) special = true;
+                Console.WriteLine("{0} -> {1}", num, special);
+            }
+
+        }
+    }
+}
