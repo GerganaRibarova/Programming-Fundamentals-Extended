25061430199e1c59fb2bfebeb5f5240a4886e845
GerganaRibarova <geri.ribarova@gmail.com>
2017-01-31 15:57:58 +0200

    Updated DataTypes-Exercises

--- DataTypes-Exercises/17.CypherRoulette/Program.cs
+++ DataTypes-Exercises/17.CypherRoulette/Program.cs
@@ -6,6 +6,21 @@ namespace _17.CypherRoulette
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            string finalText = "";
+            string areSame = "";
+
+            for(int i = 0; i < n; i ++)
+            {
+                string word = Console.ReadLine();
+                if(word == areSame)
+                {
+                    finalText = "";
+                }
+
+                areSame = word;
+
+            }
         }
     }
 }
